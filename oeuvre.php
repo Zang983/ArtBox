016482a2cd652f97acdfcfce32a04022e8ecbfbd
Kevin KERKAR <kerkar.kevin@gmail.com>
2024-05-17 17:15:30 +0200

    Fetch d'une oeuvre

--- oeuvre.php
+++ oeuvre.php
@@ -1,38 +1,37 @@
 <?php
-    require 'header.php';
-    require 'oeuvres.php';
+require 'header.php';
+require 'oeuvres.php';
+require_once 'mysql.php';
 
-    // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
-    if(empty($_GET['id'])) {
-        header('Location: index.php');
-    }
+// Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
+if (empty($_GET['id'])) {
+    header('Location: index.php');
+}
 
-    $oeuvre = null;
+$requete = 'SELECT * FROM oeuvres WHERE id = :id';
 
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
+$getOeuvre = $mysqlClient->prepare($requete);
+$getOeuvre->execute([
+    'id' => $_GET['id']
+]);
+$oeuvre = $getOeuvre->fetchAll()[0];
 
-    // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
-    if(is_null($oeuvre)) {
-        header('Location: index.php');
-    }
+
+// Si aucune oeuvre trouvé, on redirige vers la page d'accueil
+if (is_null($oeuvre)) {
+    header('Location: index.php');
+}
 ?>
 
 <article id="detail-oeuvre">
     <div id="img-oeuvre">
-        <img src="<?= $oeuvre['image'] ?>" alt="<?= $oeuvre['titre'] ?>">
+        <img src="<?= $oeuvre['urlImage'] ?>" alt="<?= $oeuvre['titre'] ?>">
     </div>
     <div id="contenu-oeuvre">
         <h1><?= $oeuvre['titre'] ?></h1>
         <p class="description"><?= $oeuvre['artiste'] ?></p>
         <p class="description-complete">
-             <?= $oeuvre['description'] ?>
+            <?= $oeuvre['description'] ?>
         </p>
     </div>
 </article>
